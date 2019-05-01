e1b9e6b12930c1a6f851eff7dc30511636c6fba1
mpartee123 <m_partee@hotmail.com>
2019-05-01 15:15:50 -0500

    Got the state "Clicked" working

--- boondocks-clicky-game/src/App.js
+++ boondocks-clicky-game/src/App.js
@@ -13,28 +13,60 @@ class App extends Component {
     };
 
     removeFriend = id => {
+        //add score to state above, get score from state, add one to it, then use it to setstate below
+
         // Filter this.state.friends for friends with an id not equal to the id being removed
-        const friends = this.state.friends.filter(friend => friend.id !== id);
+        //const friends = this.state.friends.filter(friend => friend.id !== id);
+        let newFriends = [];
+        this.state.friends.forEach(friend => {
+            let clicked = false;
+            if(friend.id === id || friend.clicked === true)
+            {
+                clicked = true
+            }
+            let newFriend = {
+                name: friend.name,
+                image: friend.image,
+                id: friend.id,
+                clicked
+            }
+
+            newFriends.push(newFriend);
+        });
+        //Below this comment, shuffle the newFriends array
+
         // Set this.state.friends equal to the new friends array
-        this.setState({ friends });
+        this.setState({
+            friends: newFriends
+            //score
+        });
+        console.log('remove friend ran and this is the info passed',friends);
     };
+
+    gameOver(){
+        alert("You lose.");
+    }
+
+    renderCards(){
+        return this.state.friends.map(friend => (
+            <ImageCard
+                removeFriend={this.removeFriend}
+                id={friend.id}
+                key={friend.id}
+                name={friend.name}
+                image={friend.image}
+                clicked={null || friend.clicked}
+                gameOver={this.gameOver}
+            />
+        ))
+    }
 
     // Map over this.state.friends and render a FriendCard component for each friend object
     render() {
         return (
             <Wrapper>
-                <Title>Friends List</Title>
-                {this.state.friends.map(friend => (
-                    <ImageCard
-                        removeFriend={this.removeFriend}
-                        id={friend.id}
-                        key={friend.id}
-                        name={friend.name}
-                        image={friend.image}
-                        occupation={friend.occupation}
-                        location={friend.location}
-                    />
-                ))}
+                <Title>Residents of woodcrest</Title>
+                {this.renderCards()}
             </Wrapper>
         );
     }
--- boondocks-clicky-game/src/components/ImageCard/ImageCard.js
+++ boondocks-clicky-game/src/components/ImageCard/ImageCard.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./style.css";
 
 function ImageCard(props) {
+    console.log(props);
+    function checkForLoss()
+    {
+        if(props.clicked)
+        {
+            props.gameOver();
+        } else {
+            props.removeFriend(props.id)
+        }
+    }
     return (
         <div className="card">
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
-            <span onClick={() => props.removeFriend(props.id)} className="remove">
+            <span onClick={() => checkForLoss()} className="remove">
         𝘅
       </span>
         </div>
